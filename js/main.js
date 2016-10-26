09e53f219844a22dbe9b1beb40cd78a53357c5fa
Gerald Hirsch <hirschgd@gmail.com>
2016-10-26 15:25:25 -0400

    Updated UI

--- js/main.js
+++ js/main.js
@@ -7,43 +7,45 @@
 
 var APP = APP || (function($) {
 	// Global jqwidgets theme setting
-	$.jqx.theme = 'light';
+	$.jqx.theme = 'bootstrap';
 	
 	var appObj = {};
 
-	appObj.title = 'Country Information Application';
+	appObj.title = 'World Country Information';
 	appObj.version = 1.0;
 
+	// Retrieve country data from geonames api
 	appObj.source =
     {
-        datatype: 'jsonp',
-        datafields: [
-            { name: 'countryName', type: 'string' },
-            { name: 'name', type: 'string' },
-            { name: 'population', type: 'float' },
-            { name: 'countryCode', type: 'string' },
-            { name: 'continentCode', type: 'string' },
-            { name: 'timeZoneId', map: 'timezone>timeZoneId' },
-            { name: 'lat', type: 'string' },
-            { name: 'lng', type: 'string' }
-        ],
-        url: 'http://api.geonames.org/searchJSON'
+      datatype: 'jsonp',
+      datafields: [
+        { name: 'countryName', type: 'string' },
+        { name: 'name', type: 'string' },
+        { name: 'population', type: 'float' },
+        { name: 'countryCode', type: 'string' },
+        { name: 'continentCode', type: 'string' },
+        { name: 'timeZoneId', map: 'timezone>timeZoneId' },
+        { name: 'lat', type: 'string' },
+        { name: 'lng', type: 'string' }
+      ],
+      url: 'http://api.geonames.org/searchJSON'
     };
 
     appObj.dataAdapter = new $.jqx.dataAdapter(appObj.source, 
-        {
-            formatData: function (data) {
-                $.extend(data, {
-                    featureClass: 'P',
-                    style: 'full',
-                    maxRows: 1000, // this is the max allowed rows for the free service (http://api.geonames.org/searchJSON)
-                    username: 'jqwidgets'
-                });
-                return data;
-            }
+      {
+        formatData: function (data) {
+          $.extend(data, {
+            featureClass: 'P',
+            style: 'full',
+            maxRows: 1000, // this is the max allowed rows for the free service (http://api.geonames.org/searchJSON)
+            username: 'jqwidgets'
+          });
+          return data;
         }
+      }
     );
 
+    // Initialize map options
     appObj.zoom;
     appObj.latlng = new google.maps.LatLng(38.907815, -77.042728);
     appObj.mapOptions = {
@@ -52,69 +54,113 @@ var APP = APP || (function($) {
     	mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
+    // Create map
     appObj.map = new google.maps.Map(document.getElementById('map'), appObj.mapOptions);
 
-    appObj.marker = new google.maps.Marker({
-    	position: appObj.latlng,
-    	map: appObj.map,
-    	icon: 'https://raw.githubusercontent.com/gosquared/flags/master/flags/flags/shiny/32/United-States.png'
-    });
+    // Update map view 
+    appObj.updateMap = function(lat, lng, country, countryCode, city, continent, pop) {
+  		appObj.map.panTo(new google.maps.LatLng(lat, lng));
 
-    appObj.updateMap = function(lat, lng, country) {
-		appObj.map.panTo(new google.maps.LatLng(lat, lng));
+  		var countryIcon = country;
 
-		var countryIcon = country;
+  		countryIcon = countryIcon.replace(/(^\s+|[^a-zA-Z0-9 ]+|\s+$)/g,"");   //Removes spaces
+      countryIcon = countryIcon.replace(/\s+/g, "-"); //Adds dash in place of space
 
-		countryIcon = countryIcon.replace(/(^\s+|[^a-zA-Z0-9 ]+|\s+$)/g,"");   //Removes spaces
-	    countryIcon = countryIcon.replace(/\s+/g, "-"); //Adds dash in place of space
+  		var iconContent = '<div style="width: 350px;">' +
+				'<ul class="list-group">' +
+					'<li class="list-group-item">' +
+					'<span class="badge">'+ country +'</span>' +
+						'<img src="https://raw.githubusercontent.com/gosquared/flags/master/flags/flags/shiny/64/'+ countryIcon +'.png">' +
+					'</li>' +
+					'<li class="list-group-item">' +
+					'<span class="badge">'+ city +'</span>' +
+						'City' +
+					'</li>' +
+					'<li class="list-group-item">' +
+					'<span class="badge">'+ pop +'</span>' +
+						'Population' +
+					'</li>' +
+					'<li class="list-group-item">' +
+					'<span class="badge">'+ countryCode +'</span>' +
+						'Country Code' +
+					'</li>' +
+					'<li class="list-group-item">' +
+					'<span class="badge">'+ continent +'</span>' +
+						'Continent' +
+					'</li>' +
+					'<li class="list-group-item">' +
+					'<span class="badge">'+ lat +'</span>' +
+						'Latitude' +
+					'</li>' +
+					'<li class="list-group-item">' +
+					'<span class="badge">'+ lng +'</span>' +
+						'Longitude' +
+					'</li>' +
+				'</ul>' +
+			'</div>';
 
-		appObj.marker = new google.maps.Marker({
-	    	position: new google.maps.LatLng(lat, lng),
-	    	map: appObj.map,
-	    	icon: {
-	    		url: 'https://raw.githubusercontent.com/gosquared/flags/master/flags/flags/shiny/32/'+ countryIcon +'.png',
-	    		anchor: new google.maps.Point(16, 48)
-	    	}
+  		var infoWindow = new google.maps.InfoWindow({
+  			content: iconContent
+  		});
+
+  		appObj.marker = new google.maps.Marker({
+      	position: new google.maps.LatLng(lat, lng),
+      	map: appObj.map,
+      	icon: {
+      		url: 'https://raw.githubusercontent.com/gosquared/flags/master/flags/flags/shiny/32/'+ countryIcon +'.png',
+      		anchor: new google.maps.Point(16, 48)
+      	}
+      });
+
+	    appObj.marker.addListener('click', function() {
+	    	infoWindow.open(appObj.map, appObj.marker);
 	    });
     }
 
+    // Create country data grid
     $('#countryGrid').jqxGrid(
     {
-        width: '99%',
-        source: appObj.dataAdapter,
-        columnsresize: true,
-        showfilterrow: true,
-        filterable: true,
-        pageable: true,
-        sortable: true,
-        altrows: true,
-        columns: [
-            { text: 'Country Name', datafield: 'countryName', filtertype: 'input' },
-            { text: 'City', datafield: 'name', filtertype: 'input' },
-            { text: 'Population', datafield: 'population', cellsformat: 'f', filtertype: 'number' },
-            { text: 'Country Code', datafield: 'countryCode', filtertype: 'input' },
-            { text: 'Date / Time', datafield: 'timeZoneId', filtertype: 'input',
-            	cellsrenderer: function(index, datafield, value, defaultvalue, column, rowdata) {
-            		var timedate = moment().tz(value),
-            			dateformat = timedate.format('YYYY-MM-DD') + " " + timedate.format('HH:mm:ss');
+      width: '100%',
+      source: appObj.dataAdapter,
+      columnsresize: true,
+      showfilterrow: true,
+      filterable: true,
+      pageable: true,
+      sortable: true,
+      altrows: true,
+      columns: [
+        { text: 'Country Name', datafield: 'countryName', filtertype: 'input' },
+        { text: 'City', datafield: 'name', filtertype: 'input' },
+        { text: 'Population', datafield: 'population', cellsformat: 'f', filtertype: 'number' },
+        { text: 'Country Code', datafield: 'countryCode', filtertype: 'input' },
+        { text: 'Date / Time', datafield: 'timeZoneId', filtertype: 'input',
+        	cellsrenderer: function(index, datafield, value, defaultvalue, column, rowdata) {
+        		var timedate = moment().tz(value),
+        			dateformat = timedate.format('YYYY-MM-DD') + " " + timedate.format('HH:mm:ss');
 
-            		return '<div class="jqx-grid-cell-left-align" style="margin-top: 6px;">' + dateformat + '</div>';
-            	}
-            }
-        ]
+        		return '<div class="jqx-grid-cell-left-align" style="margin-top: 6px;">' + dateformat + '</div>';
+        	}
+        }
+      ]
     });
 
+    // Fired upon selecting grid row
     $('#countryGrid').on('rowselect', function(e) {
     	var	row = e.args.row, 
     		lat = parseFloat(row.lat),
     		lng = parseFloat(row.lng),
-	    	country = row.countryName;
+	    	country = row.countryName,
+	    	countryCode = row.countryCode,
+	    	city = row.name,
+	    	continent = row.continentCode,
+	    	pop = row.population,
+	    	pop = pop.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-    	appObj.updateMap(lat, lng, country);    	
+    	appObj.updateMap(lat, lng, country, countryCode, city, continent, pop);    	
     });
 
 	console.log('App Running');
     
-    return appObj;
+  return appObj;
     
 }($));
